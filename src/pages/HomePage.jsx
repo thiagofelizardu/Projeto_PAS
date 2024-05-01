2025ee477c455608a9680c300c432b2d6774ac04
Fabricio976 <fabricioemanuel20@gmail.com>
2024-05-01 18:54:34 -0300

    Home - OK

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,71 +1,52 @@
 import React from 'react';
-import { useAuth } from '../context/AuthProvider';
-import { styled } from '@mui/material/styles';
 import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
-import Typography from '@mui/material/Typography';
-import ButtonBase from '@mui/material/ButtonBase';
-import { Container } from '@mui/material';
-
-const Img = styled('img')({
-  margin: 'auto',
-  display: 'block',
-  maxWidth: '100%',
-  maxHeight: '100%',
-});
+import { Container, Typography, Card, CardContent, CardMedia, Box, CardActionArea } from '@mui/material';
+import { useCadastro } from '../context/CadastroContext';
 
 const HomePage = () => {
-  const { userName } = useAuth();
+  const { cadastro } = useCadastro();
 
   return (
     <Container>
-      <Paper
-      sx={{
-        p: 2,
-        margin: 'auto',
-        maxWidth: 1500,
+      <Box sx={{
         flexGrow: 1,
-        marginTop:2,
-        marginBottom: 2,
-        padding:'80px 140px 80px 40px',
-        backgroundColor: '#a3ceef',
-      }}
-    >
-      <Grid container spacing={2}>
-        <Grid item>
-          <ButtonBase sx={{ width: 400, height: 300 }}>
-            <Img alt="complex" src="src\imagens\casa1.jpg" style={{ width: '400px', height: '400px' }} />
-          </ButtonBase>
-        </Grid>
-        <Grid item xs={12} sm container>
-          <Grid item xs container direction="column" spacing={2}>
-            <Grid item xs>
-              <Typography gutterBottom variant="subtitle1" component="div">
-                Standard license 
-              </Typography>
-              <Typography variant="body2" gutterBottom>
-                Full resolution 1920x1080 • JPEG
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                ID: 1030114
-              </Typography>
-            </Grid>
-            <Grid item>
-              <Typography sx={{ cursor: 'pointer' }} variant="body2">
-                Alugar
-              </Typography>
-            </Grid>
-          </Grid>
-          <Grid item>
-            <Typography variant="subtitle1" component="div">
-              $19.00
-            </Typography>
+        marginTop: 3,
+      }}>
+        <Grid>
+          <Grid container spacing={2}>
+            {cadastro.map((item, index) => (
+              <Grid item key={index} xs={12} sm={6} md={4}>
+                <Card >
+                  <CardActionArea>
+                    <CardMedia
+                      component="img"
+                      image={URL.createObjectURL(item.imagens)}
+                      alt="Imagem"
+                      style={{ width: '100%', height: 200, objectFit: 'cover' }}
+                    />
+                    <CardContent sx={{ flexGrow: 1 }}>
+                      <Typography variant="h5" component="div">
+                        {item.location}
+                      </Typography>
+                      <Typography variant="body2" color="text.secondary">
+                        {item.info}
+                      </Typography>
+                      <Typography variant="body2" color="text.secondary">
+                        {item.description}
+                      </Typography>
+                      <Typography fontSize='' fontWeight="bold">
+                        {`${item.price}`}
+                      </Typography>
+                    </CardContent>
+                  </CardActionArea>
+                </Card>
+              </Grid>
+            ))}
           </Grid>
         </Grid>
-      </Grid>
-    </Paper>
+      </Box>
     </Container>
-    
   );
 };
 
